dccdc09fc290e0d5eb4d80c709fa0913e9e3c19b
Nelio Alves <acenelio@gmail.com>
2018-08-20 21:00:48 -0300

    Initial implementation

---
+++ Course/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Course.Entities {
+    class Product {
+
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity) {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total() {
+            return Price * Quantity;
+        }
+    }
+}
---
+++ Course/Program.cs
@@ -0,0 +1,42 @@
+﻿using Course.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath)) {
+                    foreach (string line in lines) {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e) {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
